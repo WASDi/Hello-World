856ee67ed21d9f0359a230e9a67dfb300792f677
Alexander Gunnarsson <sir_alex67@hotmail.com>
2011-06-24 14:17:07 +0200

    adding files

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+/**
+ *
+ * @author WASD
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+
+    }
+
+}
