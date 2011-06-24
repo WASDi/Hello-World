26ac3bcc76a98fbd7f76dd9e03faf2d32919110e
Alexander Gunnarsson <sir_alex67@hotmail.com>
2011-06-24 14:28:33 +0200

    added a new class

---
+++ src/helloworld/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloworld;
+
+/**
+ *
+ * @author WASD
+ */
+public class NewClass {
+
+    private int x;
+
+    public NewClass(int x) {
+        this.x = x;
+    }
+
+}
