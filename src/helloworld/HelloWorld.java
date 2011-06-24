45a8fd82ae8e21f70f37b7727be77680de1d4c23
Patrik Weibull <patrik@patrik-desktop>
2011-06-24 15:56:35 +0200

    Changes output text

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -7,8 +7,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("hello world");
-
+	System.out.println("Hello World!");
         NewClass x = new NewClass(5);
 
     }
