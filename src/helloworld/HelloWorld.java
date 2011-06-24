e35ed199b760633c4ee62f07b65ba713eecfbf04
Alexander Gunnarsson <sir_alex67@hotmail.com>
2011-06-24 19:15:26 +0200

    replaced tabs with 4 spaces

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -7,9 +7,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-		System.out.println("Hello World!");
-		System.out.println("kuk");
-		System.out.println("kukar");
+        System.out.println("Hello World!");
+        System.out.println("kuk");
+        System.out.println("kukar");
         NewClass x = new NewClass(5);
 
     }
