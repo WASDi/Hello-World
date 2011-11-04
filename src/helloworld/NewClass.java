de3b92be89b90f83ddb89244c0628624761a33f2
Alexander Gunnarsson <sir_alex67@hotmail.com>
2011-11-04 23:10:49 +0100

    NewClass now has a name

--- src/helloworld/NewClass.java
+++ src/helloworld/NewClass.java
@@ -7,13 +7,23 @@ package helloworld;
 public class NewClass {
 
     private int x;
+    private String name;
 
     public NewClass(int x) {
+        this(x,"None");
+    }
+
+    public NewClass(int x, String name){
         this.x = x;
+        this.name = name;
     }
 
     public int getX() {
         return x;
+    }
+
+    public String getName(){
+        return name;
     }
 
 }
